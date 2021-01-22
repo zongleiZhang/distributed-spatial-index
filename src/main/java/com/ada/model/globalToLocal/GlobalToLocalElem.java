2907c809b93c0960af19d787e589ce8b1da3eff8
zzl <654310097@qq.com>
2021-01-22 09:01:21 +0800

    alter

--- src/main/java/com/ada/model/globalToLocal/GlobalToLocalElem.java
+++ src/main/java/com/ada/model/globalToLocal/GlobalToLocalElem.java
@@ -14,8 +14,8 @@ public class GlobalToLocalElem implements Serializable {
      * 元素类型：
      *  1: 索引项，value是Segment对象
      *  2: 查询项，value是QueryItem对象
-     *  3: 局部索引Region调整，value是AdjustLocalRegion,
-     *          当value是AdjustLocalRegion的region成员是null时表示废弃subtask
+     *  3: 局部索引Region调整，value是LocalRegionAdjustInfo,
+     *          当LocalRegionAdjustInfo的region成员是null时表示废弃subtask
      */
     public int elementType;
 
